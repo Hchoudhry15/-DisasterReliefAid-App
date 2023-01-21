29ed3ef315727e18c4b8ad5d9e0c99b6d9e78963
JohnRamberger <jramberger3@gatech.edu>
2023-01-20 21:24:41 -0500

    Update PasswordField.component.dart
    added validation

--- disaster_relief_aid_flutter/lib/component/PasswordField.component.dart
+++ disaster_relief_aid_flutter/lib/component/PasswordField.component.dart
@@ -1,29 +1,43 @@
+import 'package:disaster_relief_aid_flutter/DRA.config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class PasswordField extends StatelessWidget {
   const PasswordField(
-      {required this.checkStrength, required this.onChanged, super.key});
+      {required this.checkStrength, required this.onSaved, super.key});
 
   /// Whether the user is creating a new password or entering an existing one.
   /// If `true`, the password will be checked for strength.
-  final bool? checkStrength;
+  final bool checkStrength;
 
-  /// Called when the user changes the value of the password field.
-  /// The password is passed as a parameter.
-  final dynamic Function(String password) onChanged;
+  /// Called when the form is submitted.
+  /// If `checkStrength` is `true`, the password will be checked for strength.
+  ///
+  final dynamic Function(String password) onSaved;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       obscureText: true,
       decoration: const InputDecoration(
         border: OutlineInputBorder(),
         labelText: 'Password',
       ),
-      onChanged: ((value) {
-        onChanged(value);
+      onSaved: ((pwd) {
+        if (pwd != null && pwd.isNotEmpty) onSaved(pwd);
+      }),
+      validator: ((pwd) {
+        if (pwd == null || pwd.isEmpty) {
+          return 'Please enter a password';
+        }
+
+        if (checkStrength) {
+          // check password strength
+          return Config.passwordValidator(pwd);
+        } else {
+          return null;
+        }
       }),
     );
   }
