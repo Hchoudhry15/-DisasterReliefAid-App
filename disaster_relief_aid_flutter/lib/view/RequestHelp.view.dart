a6664835bdc55cb8a427814049c15fe0c53abc86
JohnRamberger <jramberger3@gatech.edu>
2023-02-20 12:55:37 -0500

    added validation to request details

--- disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
+++ disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
@@ -11,46 +11,71 @@ class RequestHelpView extends StatefulWidget {
 class _RequestHelpViewState extends State<RequestHelpView> {
   String requestDetails = "";
 
+  // make a key for the form
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Request Help"),
         ),
-        body: Column(
-          children: [
-            Card(
-                child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.place),
-                    title: Text('Your current location will be shared'),
+        body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Card(
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.place),
+                        title: Text('Your current location will be shared'),
+                      ),
+                    ])),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Please describe your situation',
                   ),
-                ])),
-            TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Please describe your situation',
-              ),
-              maxLines: 5,
-              onChanged: (value) {
-                setState(() {
-                  requestDetails = value;
-                });
-              },
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  // TODO: do something with the request details
-                  // TODO: make the request to the backend
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (c) => HelpCallInProgressView()));
-                },
-                child: const Text("Request Help"))
-          ],
-        ));
+                  maxLines: 5,
+                  onChanged: (value) {
+                    setState(() {
+                      requestDetails = value;
+                    });
+                  },
+                  onSaved: (newValue) {
+                    if (newValue == null || newValue.isEmpty) {
+                      return;
+                    }
+                    setState(() {
+                      requestDetails = newValue;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      // do form validation
+                      if (!_formKey.currentState!.validate()) {
+                        return;
+                      }
+                      _formKey.currentState!.save();
+
+                      // TODO: do something with the request details
+                      // TODO: make the request to the backend
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (c) => HelpCallInProgressView()));
+                    },
+                    child: const Text("Request Help"))
+              ],
+            )));
   }
 }
