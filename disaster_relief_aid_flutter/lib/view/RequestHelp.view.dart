b573fa2bb0fca25d38e058c0ebada95e2300bb07
JohnRamberger <jramberger3@gatech.edu>
2023-02-20 12:45:35 -0500

    Update RequestHelp.view.dart

--- disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
+++ disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:disaster_relief_aid_flutter/view/HelpCallInProgress.view.dart';
+
 class RequestHelpView extends StatelessWidget {
   const RequestHelpView({super.key});
 
@@ -11,12 +12,28 @@ class RequestHelpView extends StatelessWidget {
         ),
         body: Column(
           children: [
-            // const Text("test"),
-            // const Text("testt"),
+            Card(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.place),
+                    title: Text('Your current location will be shared'),
+                  ),
+                ])),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Please describe your situation',
+              ),
+              maxLines: 5,
+            ),
             ElevatedButton(
-                onPressed: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (c) => HelpCallInProgressView())),
-                child: const Text("Check Status"))
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (c) => HelpCallInProgressView())),
+                child: const Text("Request Help"))
           ],
         ));
   }
