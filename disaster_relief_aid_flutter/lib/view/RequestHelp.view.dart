37e6ca68e340a61c7111d50b083dc0f60c983f57
acoustic-git-tar <zehooves@gmail.com>
2023-02-20 17:56:49 -0500

    resolve merge conflicts

--- disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
+++ disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:disaster_relief_aid_flutter/view/HelpCallInProgress.view.dart';
+import 'package:geolocator/geolocator.dart';
 
 class RequestHelpView extends StatefulWidget {
   const RequestHelpView({super.key});
@@ -69,6 +70,7 @@ class _RequestHelpViewState extends State<RequestHelpView> {
 
                       // TODO: do something with the request details
                       // TODO: make the request to the backend
+                      toProgress(context);
                       Navigator.push(
                           context,
                           MaterialPageRoute(
@@ -77,5 +79,40 @@ class _RequestHelpViewState extends State<RequestHelpView> {
                     child: const Text("Request Help"))
               ],
             )));
+  }
+}
+
+toProgress(context) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (c) => HelpCallInProgressView()));
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
--- disaster_relief_aid_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ disaster_relief_aid_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_crashlytics
 import firebase_database
 import firebase_messaging
 import firebase_storage
+import geolocator_apple
 import package_info_plus_macos
 import path_provider_macos
 
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
