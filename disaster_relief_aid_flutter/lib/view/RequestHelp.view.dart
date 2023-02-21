f9028f6d698d7c78e4ef543ef87215268f34e14f
JohnRamberger <jramberger3@gatech.edu>
2023-02-20 22:57:34 -0500

    Update RequestHelp.view.dart

--- disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
+++ disaster_relief_aid_flutter/lib/view/RequestHelp.view.dart
@@ -27,58 +27,83 @@ class _RequestHelpViewState extends State<RequestHelpView> {
             key: _formKey,
             child: Column(
               children: [
-                Card(
-                    child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: const <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.place),
-                        title: Text('Your current location will be shared'),
+                const SizedBox(height: 20),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Card(
+                        child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const <Widget>[
+                          ListTile(
+                            leading: Icon(Icons.place),
+                            title: Text('Your current location will be shared'),
+                          ),
+                        ]))),
+                const SizedBox(height: 20),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Please describe your situation',
                       ),
-                    ])),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Please describe your situation',
-                  ),
-                  maxLines: 5,
-                  onChanged: (value) {
-                    setState(() {
-                      requestDetails = value;
-                    });
-                  },
-                  onSaved: (newValue) {
-                    if (newValue == null || newValue.isEmpty) {
-                      return;
-                    }
-                    setState(() {
-                      requestDetails = newValue;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      // do form validation
-                      if (!_formKey.currentState!.validate()) {
-                        return;
-                      }
-                      _formKey.currentState!.save();
+                      maxLines: 5,
+                      onChanged: (value) {
+                        setState(() {
+                          requestDetails = value;
+                        });
+                      },
+                      onSaved: (newValue) {
+                        if (newValue == null || newValue.isEmpty) {
+                          return;
+                        }
+                        setState(() {
+                          requestDetails = newValue;
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    )),
+                const SizedBox(height: 20),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: InkWell(
+                      child: Container(
+                        padding: const EdgeInsets.all(25),
+                        // ignore: prefer_const_constructors
+                        decoration: BoxDecoration(
+                            color: Colors.greenAccent,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Center(
+                          child: Text(
+                            "Request Help",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      onTap: () async {
+                        // do form validation
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        }
+                        _formKey.currentState!.save();
 
-                      // TODO: do something with the request details
-                      // TODO: make the request to the backend
-                      toProgress(context);
-                      // Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //         builder: (c) => HelpCallInProgressView()));
-                    },
-                    child: const Text("Request Help"))
+                        // TODO: do something with the request details
+                        // TODO: make the request to the backend
+                        toProgress(context);
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (c) => HelpCallInProgressView()));
+                      },
+                    ))
               ],
             )));
   }
