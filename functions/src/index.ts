b4c9a940b0e0e9499003aa3ab657cc6f590a4377
JohnRamberger <jramberger3@gatech.edu>
2023-03-11 17:43:20 -0500

    distance checking

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import {initializeApp} from "firebase-admin";
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+import { getDistance } from "geolib";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -14,16 +15,11 @@ import {initializeApp} from "firebase-admin";
 
 export const helpRequestMade = functions.database
   .ref("/requesthelplist/{userId}")
-  .onCreate( async (snapshot, context) => {
+  .onCreate(async (snapshot, context) => {
     const userId = context.params.userId;
     const location = snapshot.child("location");
     const timestamp = snapshot.child("timestamp");
 
-    functions.logger.log("userId", userId);
-    functions.logger.log("location", location);
-    functions.logger.log("timestamp", timestamp);
-
-    initializeApp();
     const db = admin.database();
 
     // get activeVolunteerList
@@ -32,11 +28,20 @@ export const helpRequestMade = functions.database
       const volunteerId = volunteer.key;
       const volunteerLocation = volunteer.child("location");
       const volunteerTimestamp = volunteer.child("timestamp");
-
-      functions.logger.log("volunteerId", volunteerId);
-      functions.logger.log("volunteerLocation", volunteerLocation);
-      functions.logger.log("volunteerTimestamp", volunteerTimestamp);
       // check if volunteer is available
+
+      // get distance between volunteer and user
+      const distance = getDistance(
+        {
+          latitude: location.child("latitude").val(),
+          longitude: location.child("longitude").val(),
+        },
+        {
+          latitude: volunteerLocation.child("latitude").val(),
+          longitude: volunteerLocation.child("longitude").val(),
+        }
+      );
+      functions.logger.debug("distance", distance);
     });
 
     return null;
