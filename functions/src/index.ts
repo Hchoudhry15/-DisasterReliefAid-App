dd10afbefa5551f3f0f8839f1c6c6ab36cb5d318
JohnRamberger <jramberger3@gatech.edu>
2023-03-11 12:54:48 -0500

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -12,8 +13,27 @@ import * as functions from "firebase-functions";
 
 export const helpRequestMade = functions.database
   .ref("/requesthelplist/{userId}")
-  .onCreate((snapshot, context) => {
+  .onCreate( async (snapshot, context) => {
     const userId = context.params.userId;
     const location = snapshot.child("location");
+    const timestamp = snapshot.child("timestamp");
+
+    const db = admin.database();
+
+    // get activeVolunteerList
+    const activeVolunteerList = await db.ref("/activevolunteerlist").get()
+    activeVolunteerList.forEach((volunteer) => {
+      const volunteerId = volunteer.key;
+      const volunteerLocation = volunteer.child("location");
+      const volunteerTimestamp = volunteer.child("timestamp");
+
+      functions.logger.log("volunteerId", volunteerId);
+      functions.logger.log("volunteerLocation", volunteerLocation);
+      functions.logger.log("volunteerTimestamp", volunteerTimestamp);
+
+      // check if volunteer is available
+      
+    });
+
     return null;
   });
