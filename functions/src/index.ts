09d191c69524e6ac51dd9a47393113a95301dbb0
JohnRamberger <jramberger3@gatech.edu>
2023-03-11 19:37:10 -0500

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import functions = require("firebase-functions");
-import admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
-import {getDistance} from "geolib";
+// import admin = require("firebase-admin");
+
+// import {getDistance} from "geolib";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -15,40 +15,66 @@ import {getDistance} from "geolib";
 
 export const helpRequestMade = functions.database
   .ref("/requesthelplist/{userId}")
-  .onCreate(async (snapshot, context) => {
+  .onCreate((snapshot, context) => {
+    // const ref = snapshot.ref;
+
+    functions.logger.log(JSON.stringify(snapshot.toJSON()));
+    return 0;
+
+    // const app = admin.initializeApp(functions.config().firebase);
+
+    // const deleteApp = () => app.delete().catch(() => null);
+
+    // app
+    //   .database()
+    //   .ref(ref)
+    //   .get()
+    //   .then((snapshot) => {
+    //     functions.logger.log(JSON.stringify(snapshot.toJSON()));
+    //     deleteApp();
+    //     return null;
+    //   })
+    //   .catch((err) => {
+    //     functions.logger.error(err);
+    //     deleteApp();
+    //     return null;
+    //   });
+
     // const userId = context.params.userId;
-    const location = snapshot.child("location");
+    // const location = snapshot.child("location");
     // const timestamp = snapshot.child("timestamp");
 
-    const latitude = location.child("latitude").val();
-    const longitude = location.child("longitude").val();
+    // functions.logger.log(snapshot.val());
 
-    functions.logger.debug("latitude", latitude);
-    functions.logger.debug("longitude", longitude);
+    // const latitude = location.child("latitude").val();
+    // const longitude = location.child("longitude").val();
 
-    const db = admin.database();
+    // functions.logger.log("latitude", latitude);
+    // functions.logger.log("longitude", longitude);
 
-    // get activeVolunteerList
-    const activeVolunteerList = await db.ref("/activevolunteerlist").get();
-    activeVolunteerList.forEach((volunteer) => {
-      // const volunteerId = volunteer.key;
-      const volunteerLocation = volunteer.child("location");
-      // const volunteerTimestamp = volunteer.child("timestamp");
-      // check if volunteer is available
+    // admin.initializeApp(functions.config().firebase);
 
-      // get distance between volunteer and user
-      const distance = getDistance(
-        {
-          latitude: latitude,
-          longitude: longitude,
-        },
-        {
-          latitude: volunteerLocation.child("latitude").val(),
-          longitude: volunteerLocation.child("longitude").val(),
-        }
-      );
-      functions.logger.debug("distance", distance);
-    });
+    // const db = admin.database();
 
-    return null;
+    // // get activeVolunteerList
+    // const activeVolunteerList = await db.ref("/activevolunteerlist").get();
+    // activeVolunteerList.forEach((volunteer) => {
+    //   // const volunteerId = volunteer.key;
+    //   // const volunteerLocation = volunteer.child("location");
+    //   // // const volunteerTimestamp = volunteer.child("timestamp");
+    //   // // check if volunteer is available
+
+    //   // // get distance between volunteer and user
+    //   // const distance = getDistance(
+    //   //   {
+    //   //     latitude: latitude,
+    //   //     longitude: longitude,
+    //   //   },
+    //   //   {
+    //   //     latitude: volunteerLocation.child("latitude").val(),
+    //   //     longitude: volunteerLocation.child("longitude").val(),
+    //   //   }
+    //   // );
+    //   // functions.logger.debug("distance", distance);
+    // });
   });
