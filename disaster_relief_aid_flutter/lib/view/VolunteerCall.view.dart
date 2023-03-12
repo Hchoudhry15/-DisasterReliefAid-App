d1ebe5fc3c5b2bb0fe1a173d4b435538539c2e94
JohnRamberger <jramberger3@gatech.edu>
2023-03-12 03:49:22 -0400

    Create VolunteerCall.view.dart

---
+++ disaster_relief_aid_flutter/lib/view/VolunteerCall.view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class VolunteerCallView extends StatefulWidget {
+  const VolunteerCallView({super.key});
+
+  @override
+  State<VolunteerCallView> createState() => _VolunteerCallViewState();
+}
+
+class _VolunteerCallViewState extends State<VolunteerCallView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Volunteer Call"),
+          automaticallyImplyLeading: false,
+        ),
+        body: const Text("Please navigate to the User in distress."));
+  }
+}
