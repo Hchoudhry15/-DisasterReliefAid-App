e6b400c8cce59d42e5780bb438c8d5984aec1e77
JohnRamberger <jramberger3@gatech.edu>
2023-03-12 04:30:39 -0400

    Update HelpCallInProgressWrapper.dart

--- disaster_relief_aid_flutter/lib/component/HelpCallInProgressWrapper.dart
+++ disaster_relief_aid_flutter/lib/component/HelpCallInProgressWrapper.dart
@@ -96,6 +96,10 @@ class _HelpCallInProgressWrapperState extends State<HelpCallInProgressWrapper> {
         Navigator.pop(context);
 
         // TODO: go to help call in progress screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const VolunteerCallView()),
+        );
       },
     );
 
