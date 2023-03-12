7b85265b63703d476fd8b0d9942c130914702e2f
JohnRamberger <jramberger3@gatech.edu>
2023-03-12 04:04:20 -0400

    Update helprequest.model.dart

--- disaster_relief_aid_flutter/lib/model/helprequest.model.dart
+++ disaster_relief_aid_flutter/lib/model/helprequest.model.dart
@@ -2,6 +2,12 @@ class HelpRequest {
   final String message;
   final String uid;
   final String distance;
+  final String longitude;
+  final String latitude;
   HelpRequest(
-      {required this.message, required this.uid, required this.distance});
+      {required this.message,
+      required this.uid,
+      required this.distance,
+      required this.longitude,
+      required this.latitude});
 }
