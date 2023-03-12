b3b0366e5279e8e1a8d0996dbae50c5b0df1c6ee
JohnRamberger <jramberger3@gatech.edu>
2023-03-12 03:39:44 -0400

    Create HelpCallInProgressWrapper.dart

---
+++ disaster_relief_aid_flutter/lib/component/HelpCallInProgressWrapper.dart
@@ -0,0 +1,43 @@
+import 'package:disaster_relief_aid_flutter/singletons/Volunteering.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HelpCallInProgressWrapper extends StatefulWidget {
+  const HelpCallInProgressWrapper({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  State<HelpCallInProgressWrapper> createState() =>
+      _HelpCallInProgressWrapperState();
+}
+
+class _HelpCallInProgressWrapperState extends State<HelpCallInProgressWrapper> {
+  bool activeHelpRequest = VolunteeringSingleton().currentHelpRequest != null;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    VolunteeringSingleton().onHelpRequestAcceptedStream.listen((event) {
+      print("accepted help request");
+      setState(() {
+        activeHelpRequest = VolunteeringSingleton().currentHelpRequest != null;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: activeHelpRequest == true
+          ? AppBar(
+              title: const Text("Help Request In Progress"),
+              backgroundColor: Colors.red,
+            )
+          : null,
+      body: widget.child,
+    );
+  }
+}
