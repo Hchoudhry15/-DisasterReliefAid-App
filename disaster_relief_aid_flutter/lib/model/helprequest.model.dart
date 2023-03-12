e710d4a2afb7fb90045185caba02a1b652af558e
JohnRamberger <jramberger3@gatech.edu>
2023-03-12 03:13:32 -0400

    Create helprequest.model.dart

---
+++ disaster_relief_aid_flutter/lib/model/helprequest.model.dart
@@ -0,0 +1,7 @@
+class HelpRequest {
+  final String message;
+  final String uid;
+  final String distance;
+  HelpRequest(
+      {required this.message, required this.uid, required this.distance});
+}
